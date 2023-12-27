83e6d2f21dc7b5002eb719e2c07fe1acde87d867
ahnafi <devsulthon@gmail.com>
2023-12-27 21:09:10 +0700

    request url test

---
+++ test/request-url.test.js
@@ -0,0 +1,24 @@
+import express from "express";
+import request from "supertest";
+
+const app = express();
+
+app.get("/hello/world", (req, res) => {
+  res.json({
+    path: req.path,
+    originalUrl: req.originalUrl,
+    hostname: req.hostname,
+    protocol: req.protocol,
+  });
+});
+
+test("request url", async () => {
+  const response = await request(app).get("/hello/world").query({ name: "world" });
+
+  expect(response.body).toEqual({
+    path: "/hello/world",
+    originalUrl: "/hello/world?name=world",
+    hostname: "127.0.0.1",
+    protocol: "http",
+  });
+});
